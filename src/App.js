eb9016912b4f893c7c6c34ba52880c3ca45c60c5
mitaldodia <mdodia@kernshell.com>
2022-11-28 15:55:24 +0530

    functionality

--- src/App.js
+++ src/App.js
@@ -1,204 +1,292 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Container } from 'react-bootstrap';
-import Table from 'react-bootstrap/Table'; 
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 const client = axios.create({
-	baseURL: 'https://jsonplaceholder.typicode.com/users',
+  baseURL: "https://jsonplaceholder.typicode.com/users",
 });
 
+const initObj = {
+  name: "",
+  username: "",
+  email: "",
+  phone: "",
+  website: "",
+};
+
 const App = () => {
-	const [id, setID] = useState('');
-	const [name, setName] = useState('');
-	const [username, setUserName] = useState('');
-	const [email, setEmail] = useState('');
-	const [phone, setPhone] = useState('');
-	const [website, setWebsite] = useState('');
-	const [posts, setPosts] = useState([]);
+  const [id, setID] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [empModal, setEmpModal] = useState(initObj);
 
-	// GET with Axios
-	useEffect(() => {
-		const fetchPost = async () => {
-			try {
-				let response = await client.get('?_limit=10');
-				setPosts(response.data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchPost();
-	}, []);
+  // GET with Axios
+  useEffect(() => {
+    fetchPost();
+  }, []);
 
-	// DELETE with Axios
-	const deletePost = async (id) => {
-		try {
-			await client.delete(`${id}`);
-			setPosts(
-				posts.filter((post) => {
-					return post.id !== id;
-				})
-			);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const fetchPost = async () => {
+    try {
+      let response = await client.get();
+      setPosts(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	// handle form submission
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		addPosts(name, username, email, phone, website);
-	};
+  // DELETE with Axios
+  const deletePost = async (id) => {
+    if (window.confirm("Do you want to remove Employee detail ?")) {
+      try {
+        await client.delete(`${id}`);
+        const foundIndex = posts.findIndex((item) => item.id == empModal.id);
+        posts.splice(foundIndex, 1);
+        setPosts([...posts]);
+		console.log(posts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
-	// POST with Axios
-	const addPosts = async (name, username, email, phone, website) => {
-		try {
-			let response = await client.post('', {
-				name: name,
-				username: username,
-				email: email,
-				phone: phone,
-				website: website
-			});
-			setPosts([response.data, ...posts]);
-			setName('');
-			setUserName('');
-			setEmail('');
-			setPhone('');
-			setWebsite('');
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  // handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-	// modal state 
-	const [show, setShow] = useState(false);
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+  // POST with Axios
+  const onSave = async () => {
+    try {
+      const mode = !empModal.id ? "add" : "edit";
+      console.log(empModal);
+      if (mode === "add") {
+        // let response = await client.post(empModal);
+		posts.push({ ...empModal, id: posts.length + 1 })
+      } else {
+        // let response = await client.put(empModal);
+        const foundIndex = posts.findIndex((item) => item.id == empModal.id);
+		posts.splice(foundIndex, 1, empModal)
+	}
+		console.log(posts);
+		setPosts([...posts]);
+      setEmpModal(initObj);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return (
-		<>
-			<nav className='title-name'>
-				<h3>Employee Details</h3>
-			</nav>
-			<Container>
-				<div className="app">
-				
-				{/* modal start here  */}
-					<Modal show={show} onHide={handleClose}>
-						<Modal.Header closeButton>
-						<Modal.Title>Employee Details Form</Modal.Title>
-						</Modal.Header>
-						<Modal.Body>
-						<div className="add-post-container">
+  // modal state
+  const [show, setShow] = useState(false);
+  const [viewDataModel, setViewDataModel] = useState(false);
+  const handleClose = () => {
+    console.log("data on submit button");
+    console.log(empModal);
+    setShow(false);
+  };
+  const handleShow = () => {
+	setEmpModal(initObj);
+	setShow(true);
+  }
+  const handleViewClose = () => {
+    setViewDataModel(false);
+  };
 
-							<form className='row form-group' onSubmit={handleSubmit}>
-								<div className='col-6 my-3'>
-									<label>Enter First Name</label>
+  const viewData = (data) => {
+    console.log(" view data info: ===>", posts);
+    setViewDataModel(true);
+    setEmpModal(data);
+  };
 
-										<input
-											type="text"
-											className="form-control"
-											value={name}
-											onChange={(e) => setName(e.target.value)}
-										/>
-								</div>
-								<div className='col-6 my-3'>
-									<label>Enter Last Name</label>
-										<input
-											type="text"
-											className="form-control"
-											value={username}
-											onChange={(e) => setUserName(e.target.value)}
-										/>
-								</div>
-								<div className='col-6 my-3'>
-									<label>Enter Email Id</label>
-										<input
-											type="text"
-											className="form-control"
-											value={email}
-											onChange={(e) => setEmail(e.target.value)}
-										/>
-								</div>
-								<div className='col-6 my-3'>
-									<label>Enter Phone Number</label>
-										<input
-											type="text"
-											className="form-control"
-											value={phone}
-											onChange={(e) => setPhone(e.target.value)}
-										/>
-								</div>
-								<div className='col-12 my-3'>
-									<label>Enter website</label>
-										<input
-											type="text"
-											className="form-control"
-											value={website}
-											onChange={(e) => setWebsite(e.target.value)}
-										/>
-								</div>
-								<button type="submit" onClick={handleClose}>Submite</button>
-							</form>
-						</div>
-						</Modal.Body>
-					</Modal>
-				{/* modal end here  */}
+  const onEditData = (data) => {
+    setShow(true);
+    setEmpModal(data);
+  };
 
-				
-				<div className="posts-container">
-					<div className="add-new-emp">
-						<Button variant="primary" onClick={handleShow}>
-						<img src = "./add.png" className='add-btn' alt=""/>	Add new Employee
-						</Button>
-					</div>
-					<div className='employe-header'>
-						<Table striped bordered hover>
-							<thead>
-								<tr>
-								<th>id</th>
-								<th>First Name</th>
-								<th>Last Name</th>
-								<th>Email</th>
-								<th>Phone</th>
-								<th>Website</th>
-								<th>Action</th>
-								</tr>
-							</thead>
-							
-						
-					{posts.map((post) => {
-						return (
-							<tbody key={post.id}>
-								<tr>
-								<td>{post.id}</td>
-								<td>{post.name}</td>
-								<td>{post.username}</td>
-								<td>{post.email}</td>
-								<td>{post.phone}</td>
-								<td>{post.website}</td>
-								<td>
-									<div className="button">
-									<div className="delete-btn" onClick={() => deletePost(post.id)}>
-											Delete
-										</div>
-									</div>
-								</td>
-								</tr>
-							</tbody>
-						);
-					})}
-					</Table>
-					</div>
-				</div>
-				</div>
-			</Container>
-		</>
-		
-	);
+  const onValueChange = (e) => {
+    const { name, value } = e.target;
+    setEmpModal({ ...empModal, [name]: value });
+  };
+
+  return (
+    <>
+      <nav className="title-name">
+        <h3>Employee Details</h3>
+      </nav>
+      <Container>
+        <div className="app">
+          {/* modal start here  */}
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Employee Details Form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="add-post-container">
+                <form className="row form-group" onSubmit={handleSubmit}>
+                  <div className="col-6 my-3">
+                    <label>Enter First Name</label>
+
+                    <input
+                      type="text"
+                      name="name"
+                      className="form-control"
+                      value={empModal.name}
+                      onChange={onValueChange}
+                    />
+                  </div>
+                  <div className="col-6 my-3">
+                    <label>Enter Last Name</label>
+                    <input
+                      type="text"
+                      name="username"
+                      className="form-control"
+                      value={empModal.username}
+                      onChange={onValueChange}
+                    />
+                  </div>
+                  <div className="col-6 my-3">
+                    <label>Enter Email Id</label>
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      value={empModal.email}
+                      onChange={onValueChange}
+                    />
+                  </div>
+                  <div className="col-6 my-3">
+                    <label>Enter Phone Number</label>
+                    <input
+                      type="text"
+                      name="phone"
+                      className="form-control"
+                      value={empModal.phone}
+                      onChange={onValueChange}
+                    />
+                  </div>
+                  <div className="col-12 my-3">
+                    <label>Enter website</label>
+                    <input
+                      type="text"
+                      name="website"
+                      className="form-control"
+                      value={empModal.website}
+                      onChange={onValueChange}
+                    />
+                  </div>
+                  <button type="submit" onClick={() => {
+					handleClose();
+					onSave();
+				  }}>
+                    Submite
+                  </button>
+                </form>
+              </div>
+            </Modal.Body>
+          </Modal>
+          {/* modal end here  */}
+
+          {/* view detail modal start here  */}
+          <Modal show={viewDataModel} onHide={handleViewClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Show Employee Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div className="add-post-container">
+                <form className="row form-group">
+                  <div className="col-6 my-3">
+                    <label>First Name</label>
+
+                    <p>{empModal.name}</p>
+                  </div>
+                  <div className="col-6 my-3">
+                    <label>Enter Last Name</label>
+                    <p>{empModal.username}</p>
+                  </div>
+                  <div className="col-6 my-3">
+                    <label>Enter Email Id</label>
+                    <p>{empModal.email}</p>
+                  </div>
+                  <div className="col-6 my-3">
+                    <label>Enter Phone Number</label>
+                    <p>{empModal.phone}</p>
+                  </div>
+                  <div className="col-12 my-3">
+                    <label>Enter website</label>
+                    <p>{empModal.website}</p>
+                  </div>
+                  {/* <button type="submit" onClick={handleClose}>Submite</button> */}
+                </form>
+              </div>
+            </Modal.Body>
+          </Modal>
+          {/* modal end here  */}
+
+          <div className="posts-container">
+            <div className="add-new-emp">
+              <Button variant="primary" onClick={handleShow}>
+                <img src="./add.png" className="add-btn" alt="" /> Add new
+                Employee
+              </Button>
+            </div>
+            <div className="employe-header">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Website</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+
+                {posts.map((post) => {
+                  return (
+                    <tbody key={post.id}>
+                      <tr>
+                        <td>{post.id}</td>
+                        <td>{post.name}</td>
+                        <td>{post.username}</td>
+                        <td>{post.email}</td>
+                        <td>{post.phone}</td>
+                        <td>{post.website}</td>
+                        <td>
+                          <div className="all-button">
+                            <div
+                              className="delete-btn me-2"
+                              onClick={() => viewData(post)}
+                            >
+                              View Details
+                            </div>
+                            <div
+                              className="delete-btn"
+                              onClick={() => onEditData(post)}
+                            >
+                              Edit
+                            </div>
+                            <div
+                              className="delete-btn"
+                              onClick={() => deletePost(post.id)}
+                            >
+                              Delete
+                            </div>
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+              </Table>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default App;
